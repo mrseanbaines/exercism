0d98f99e9ebd211d23d18b396a878c1de6904a37
Sean Baines <mrseanbaines@gmail.com>
2019-06-22 18:28:53 +0200

    Complete protein-translation challenge

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,8 +1,36 @@
-//
-// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const proteins = {
+  Methionine: ['AUG'],
+  Phenylalanine: ['UUU', 'UUC'],
+  Leucine: ['UUA', 'UUG'],
+  Serine: ['UCU', 'UCC', 'UCA', 'UCG'],
+  Tyrosine: ['UAU', 'UAC'],
+  Cysteine: ['UGU', 'UGC'],
+  Tryptophan: ['UGG'],
+  STOP: ['UAA', 'UAG', 'UGA'],
+};
 
-export const translate = () => {
-  throw new Error("Remove this statement and implement this function");
+const getProtein = (obj, input) => {
+  const match = Object.entries(obj).find(arr =>
+    arr[1].some(codon => codon === input),
+  );
+
+  if (match) {
+    return match[0];
+  }
+
+  throw new Error('Invalid codon');
+};
+
+export const translate = (RNA = []) => {
+  const codons = [];
+
+  for (let i = 0; i < RNA.length; i += 3) {
+    const codon = RNA.substring(i, i + 3);
+
+    if (getProtein(proteins, codon) === 'STOP') break;
+
+    codons.push(codon);
+  }
+
+  return codons.map(codon => getProtein(proteins, codon));
 };
