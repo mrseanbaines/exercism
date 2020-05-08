cf30f88500197093bdea923050a7c8d3edda5615
Sean Baines <mrseanbaines@gmail.com>
2020-05-08 08:18:47 +0100

    Complete gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function')
+export const gigasecond = (date) => {
+  const gs = 1000000000 * 1000
+
+  return new Date(Date.parse(date) + gs)
 }
