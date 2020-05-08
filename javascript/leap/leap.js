89f7529ca74968e212ba2cb5e36205a091e997d4
Sean Baines <mrseanbaines@gmail.com>
2020-05-08 09:46:31 +0100

    Update and re-download exercise

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,9 +1,7 @@
-const isDivisible = num1 => num2 => num1 % num2 === 0;
+const isDivisible = (num1) => (num2) => num1 % num2 === 0
 
-export const isLeap = year => {
-  const yearIsDivisibleBy = isDivisible(year);
+export const isLeap = (year) => {
+  const yearIsDivisibleBy = isDivisible(year)
 
-  return (
-    yearIsDivisibleBy(4) && (!yearIsDivisibleBy(100) || yearIsDivisibleBy(400))
-  );
-};
+  return yearIsDivisibleBy(4) && (!yearIsDivisibleBy(100) || yearIsDivisibleBy(400))
+}
