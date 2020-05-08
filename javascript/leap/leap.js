ef87b1b17bfb73ab96805da823733bc82c05d792
Sean Baines <mrseanbaines@gmail.com>
2020-05-08 09:50:27 +0100

    Refactor based on comments

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,7 +1,11 @@
-const isDivisible = (num1) => (num2) => num1 % num2 === 0
+const isDivisibleBy = (dividend) => {
+  return (divisor) => {
+    return dividend % divisor === 0
+  }
+}
 
 export const isLeap = (year) => {
-  const yearIsDivisibleBy = isDivisible(year)
+  const yearIsDivisibleBy = isDivisibleBy(year)
 
   return yearIsDivisibleBy(4) && (!yearIsDivisibleBy(100) || yearIsDivisibleBy(400))
 }
