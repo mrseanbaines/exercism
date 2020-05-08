9c9e3833c1e4a68bbf5f5e3d817164b1d85206b5
Sean Baines <mrseanbaines@gmail.com>
2020-05-08 12:07:25 +0100

    Complete run-length-encoding

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -3,10 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const encode = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const encode = (str) => {
+  const segments = str.match(/([\w\s])(\1+)?/g) || []
 
-export const decode = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+  if (!segments.length) {
+    return str
+  }
+
+  return segments.map((segment) => (segment.length > 1 ? segment.length : '') + segment.charAt(0)).join('')
+}
+
+export const decode = (str) => {
+  const segments = str.match(/([\d]+)?([\w+\s])/g) || []
+
+  if (!segments.length) {
+    return str
+  }
+
+  return segments
+    .map((segment) => segment.match(/([\d]+)?([\w+\s])/) || [segment])
+    .map(([match, num, char]) => (num && char ? char.repeat(num) : match))
+    .join('')
+}
