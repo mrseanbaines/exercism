4f2e6699b1e7fc36e4dfb4c7d233a6900bb78f7b
Sean Baines <mrseanbaines@gmail.com>
2020-05-08 08:24:25 +0100

    Update and re-download exercise

--- javascript/protein-translation/protein-translation.spec.js
+++ javascript/protein-translation/protein-translation.spec.js
@@ -1,72 +1,63 @@
-import { translate } from './protein-translation';
+import { translate } from './protein-translation'
 
 describe('ProteinTranslation', () => {
   test('Empty RNA has no proteins', () => {
-    expect(translate()).toEqual([]);
-  });
+    expect(translate()).toEqual([])
+  })
 
-  test('Methionine codon translates into protein', () => {
-    expect(translate('AUG')).toEqual(['Methionine']);
-  });
+  xtest('Methionine codon translates into protein', () => {
+    expect(translate('AUG')).toEqual(['Methionine'])
+  })
 
-  test('Phenylalanine codons translate into protein', () => {
-    expect(translate('UUUUUC')).toEqual(['Phenylalanine', 'Phenylalanine']);
-  });
+  xtest('Phenylalanine codons translate into protein', () => {
+    expect(translate('UUUUUC')).toEqual(['Phenylalanine', 'Phenylalanine'])
+  })
 
-  test('Leucine codons translate into protein', () => {
-    expect(translate('UUAUUG')).toEqual(['Leucine', 'Leucine']);
-  });
+  xtest('Leucine codons translate into protein', () => {
+    expect(translate('UUAUUG')).toEqual(['Leucine', 'Leucine'])
+  })
 
-  test('Serine codons translate into protein', () => {
-    expect(translate('UCUUCCUCAUCG')).toEqual([
-      'Serine',
-      'Serine',
-      'Serine',
-      'Serine',
-    ]);
-  });
+  xtest('Serine codons translate into protein', () => {
+    expect(translate('UCUUCCUCAUCG')).toEqual(['Serine', 'Serine', 'Serine', 'Serine'])
+  })
 
-  test('Tyrosine codons translate into protein', () => {
-    expect(translate('UAUUAC')).toEqual(['Tyrosine', 'Tyrosine']);
-  });
+  xtest('Tyrosine codons translate into protein', () => {
+    expect(translate('UAUUAC')).toEqual(['Tyrosine', 'Tyrosine'])
+  })
 
-  test('Cysteine codons translate into protein', () => {
-    expect(translate('UGUUGC')).toEqual(['Cysteine', 'Cysteine']);
-  });
+  xtest('Cysteine codons translate into protein', () => {
+    expect(translate('UGUUGC')).toEqual(['Cysteine', 'Cysteine'])
+  })
 
-  test('Tryptophan codon translates into protein', () => {
-    expect(translate('UGG')).toEqual(['Tryptophan']);
-  });
+  xtest('Tryptophan codon translates into protein', () => {
+    expect(translate('UGG')).toEqual(['Tryptophan'])
+  })
 
-  test('Sequence starts with stop codon 1', () => {
-    expect(translate('UAAUUUUUA')).toEqual([]);
-  });
+  xtest('Sequence starts with stop codon 1', () => {
+    expect(translate('UAAUUUUUA')).toEqual([])
+  })
 
-  test('Sequence starts with stop codon 2', () => {
-    expect(translate('UAGAUGUAU')).toEqual([]);
-  });
+  xtest('Sequence starts with stop codon 2', () => {
+    expect(translate('UAGAUGUAU')).toEqual([])
+  })
 
-  test('Sequence starts with stop codon 3', () => {
-    expect(translate('UGAUGU')).toEqual([]);
-  });
+  xtest('Sequence starts with stop codon 3', () => {
+    expect(translate('UGAUGU')).toEqual([])
+  })
 
-  test('Small RNA strand', () => {
-    expect(translate('AUGUUUUCU')).toEqual([
-      'Methionine',
-      'Phenylalanine',
-      'Serine',
-    ]);
-  });
+  xtest('Small RNA strand', () => {
+    expect(translate('AUGUUUUCU')).toEqual(['Methionine', 'Phenylalanine', 'Serine'])
+  })
 
-  test('Stop codon ends translation', () => {
-    expect(translate('AUGUUUUCUUAAAUG')).toEqual([
-      'Methionine',
-      'Phenylalanine',
-      'Serine',
-    ]);
-  });
+  xtest('Stop codon ends translation', () => {
+    expect(translate('AUGUUUUCUUAAAUG')).toEqual(['Methionine', 'Phenylalanine', 'Serine'])
+  })
 
-  test('Invalid codon throws error', () => {
-    expect(() => translate('LOL')).toThrow(new Error('Invalid codon'));
-  });
-});
+  xtest('Invalid codon throws error', () => {
+    expect(() => translate('LOL')).toThrow(new Error('Invalid codon'))
+  })
+
+  xtest('Invalid codon throws error', () => {
+    expect(() => translate('AUGOO')).toThrow(new Error('Invalid codon'))
+  })
+})
