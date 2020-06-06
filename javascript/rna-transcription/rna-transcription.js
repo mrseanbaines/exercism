5b4d6485debf1a79af80d71ac683324f62b522a0
Sean Baines <mrseanbaines@gmail.com>
2020-06-06 10:25:27 +0100

    Complete rna-transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,8 +1,13 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const toRna = (dna) => {
+  const map = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U',
+  }
 
-export const toRna = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+  return dna
+    .split('')
+    .map((nucleotide) => map[nucleotide])
+    .join('')
+}
