caed95190bb86d1961ee281141469fd7ba990c46
Sean Baines <mrseanbaines@gmail.com>
2020-06-24 19:59:03 +0100

    Remove skeleton file comments

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const gigasecond = (date) => {
   const gs = 1000000000 * 1000
 
--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Run Length Encoding' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const encode = (str) => str.replace(/([\w\s])\1+/g, (match, char) => match.length + char)
 
 export const decode = (str) => str.replace(/(\d+)([\w\s])/g, (_, num, char) => char.repeat(num))
