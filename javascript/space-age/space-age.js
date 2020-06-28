bec3419e057004a4deaadcee7811aec27b13a62d
Sean Baines <mrseanbaines@gmail.com>
2020-06-28 10:12:35 +0100

    Complete space-age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,3 +1,20 @@
-export const age = () => {
-  throw new Error('Remove this statement and implement this function')
+const earthYears = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+}
+
+const secondsToYears = seconds => {
+  const SECONDS_IN_YEAR = 31557600
+
+  return Number((seconds / SECONDS_IN_YEAR).toFixed(2))
+}
+
+export const age = (planet, ageInSeconds) => {
+  return secondsToYears(ageInSeconds / earthYears[planet])
 }
